af152da173bd601370369fd211eee402eee31846
mickyblack78 <mickyblack.78@gmail.com>
2026-03-30 00:45:26 +1000

    Update MainActivity.java

--- app/src/main/java/MainActivity.java
+++ app/src/main/java/MainActivity.java
@@ -1,1 +1,14 @@
+package com.mick.fixer;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
 
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Fixer Bridge is Active");
+        setContentView(tv);
+    }
+}
